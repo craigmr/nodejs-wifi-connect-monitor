181b0f24196a99da4732ef23c352c6990d134b74
Craig Simpson <craigmr@gmail.com>
2014-03-23 22:38:33 -0400

    Added logging.

--- server.js
+++ server.js
@@ -1,22 +1,34 @@
 (function(){
     'use strict';
-    //var io = require('socket.io')();
+    var io = require('socket.io').listen(3030);
+    var winston = require('winston');
+    winston.add(winston.transports.File, { filename: 'wifi-device.log' });
 
     var DeviceMonitor = require('./lib/monitor.js');
+
     var monitor = new DeviceMonitor('http://192.168.1.254/xslt?PAGE=C_2_0');
     monitor.start();
 
     monitor.on(DeviceMonitor.START, function onDeviceStart(devices){
-        console.log('Monitor Starting');
+        winston.info('device monitoring starting', devices);
+        io.sockets.emit('start', devices);
+    });
+
+    monitor.on(DeviceMonitor.END, function onMonitroEnd(){
+        winston.info('device monitoring ending');
     });
 
     monitor.on(DeviceMonitor.ADD, function onDeviceAdd(device){
-        console.log('Device added:');
-        console.log(device);
+        winston.info('added', device);
+        io.sockets.emit('add', device);
     });
 
     monitor.on(DeviceMonitor.REMOVE, function onDeviceRemove(device){
-        console.log('Device removed:');
-        console.log(device);
+        winston.info('removed', device);
+        io.sockets.emit('remove', device);
+    });
+
+    monitor.on(DeviceMonitor.ERROR, function onMonitorError(error){
+        winston.error(error);
     });
 })();
